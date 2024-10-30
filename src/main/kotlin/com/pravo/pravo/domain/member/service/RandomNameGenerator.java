c0cadab517fa87211ee936bbbb0e0aa01890923a
dthcs <dthcsc@gmail.com>
2024-10-30 17:19:17 +0900

    [REFACTOR] save different name

--- src/main/kotlin/com/pravo/pravo/domain/member/service/RandomNameGenerator.java
+++ src/main/kotlin/com/pravo/pravo/domain/member/service/RandomNameGenerator.java
@@ -1,45 +1,50 @@
 package com.pravo.pravo.domain.member.service;
 
+import com.pravo.pravo.domain.member.repository.MemberRepository;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 public class RandomNameGenerator {
 
-    //    private final MemberRepository memberRepository;
-//    private final Random random;
     private static final String ADJECTIVE_FILE = "src/main/kotlin/com/pravo/pravo/domain/member/resources/adjective.txt";
     private static final String NOUN_FILE = "src/main/kotlin/com/pravo/pravo/domain/member/resources/noun.txt";
+    private final MemberRepository memberRepository;
+    //    private final Random random;
+
+    public RandomNameGenerator(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public String generateUniqueRandomName() {
         String nickname = "";
-        try {
-            Resource adjectiveResource = new FileSystemResource(ADJECTIVE_FILE);
-            Resource nounResource = new FileSystemResource(NOUN_FILE);
+        do {
+            try {
+                Resource adjectiveResource = new FileSystemResource(ADJECTIVE_FILE);
+                Resource nounResource = new FileSystemResource(NOUN_FILE);
 
-            ArrayList<String> index = readFile(adjectiveResource);
-            ArrayList<String> index2 = readFile(nounResource);
+                ArrayList<String> index = readFile(adjectiveResource);
+                ArrayList<String> index2 = readFile(nounResource);
 
-            int ran1max = index.size();
-            int ran2max = index2.size();
-            Random random = new Random();
-            int ran = random.nextInt(ran1max);
-            int ran2 = random.nextInt(ran2max);
+                int ran1max = index.size();
+                int ran2max = index2.size();
+                Random random = new Random();
+                int ran = random.nextInt(ran1max);
+                int ran2 = random.nextInt(ran2max);
 
-            nickname = index.get(ran) + " " + index2.get(ran2);
-        } catch (IOException e) {
-            System.out.println("파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
-            e.printStackTrace();
-        }
-        System.out.println("nickname: " + nickname);
+                nickname =
+                    index.get(ran) + " " + index2.get(ran2) + " " + (100 + random.nextInt(900));
+            } catch (IOException e) {
+                System.out.println("파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
+                e.printStackTrace();
+            }
+            System.out.println("nickname: " + nickname);
+        } while (memberRepository.existsByName(nickname));
         return nickname;
     }
 
