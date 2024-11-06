028ab62b6791ef9ce93ad7552999e7cf762ad59d
baebae02 <bae4614@gmail.com>
2024-11-06 20:32:08 +0900

    [FIX] exception status code

--- src/main/kotlin/com/pravo/pravo/global/common/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/pravo/pravo/global/common/error/GlobalExceptionHandler.kt
@@ -13,34 +13,34 @@ class GlobalExceptionHandler {
     protected fun handleException(exception: Exception): ResponseEntity<ApiResponseDto<Nothing>> {
         val errorCode = ErrorCode.INTERNAL_SERVER_ERROR
         val response = ApiResponseDto.error(errorCode.message, errorCode.status, errorCode.code)
-        return ResponseEntity.ok(response)
+        return ResponseEntity.status(errorCode.status).body(response)
     }
 
     @ExceptionHandler(RuntimeException::class)
     fun handleRuntimeException(e: RuntimeException): ResponseEntity<ApiResponseDto<Nothing>> {
         val errorCode = ErrorCode.INTERNAL_SERVER_ERROR
         val response = ApiResponseDto.error(errorCode.message, errorCode.status, errorCode.code)
-        return ResponseEntity.ok(response)
+        return ResponseEntity.status(errorCode.status).body(response)
     }
 
     @ExceptionHandler(EntityNotFoundException::class)
     fun handleEntityNotFoundException(e: EntityNotFoundException): ResponseEntity<ApiResponseDto<Nothing>> {
         val errorCode = ErrorCode.NOT_FOUND
         val response = ApiResponseDto.error(errorCode.message, errorCode.status, errorCode.code)
-        return ResponseEntity.ok(response)
+        return ResponseEntity.status(errorCode.status).body(response)
     }
 
     @ExceptionHandler(AccessDeniedException::class)
     fun handleAuthenticationException(exception: AccessDeniedException): ResponseEntity<ApiResponseDto<Nothing>> {
         val errorCode = ErrorCode.UNAUTHORIZED
         val response = ApiResponseDto.error(errorCode.message, errorCode.status, errorCode.code)
-        return ResponseEntity.ok(response)
+        return ResponseEntity.status(errorCode.status).body(response)
     }
 
     @ExceptionHandler(UnauthorizedException::class)
     fun handleUnauthorizedException(e: UnauthorizedException): ResponseEntity<ApiResponseDto<Nothing>> {
         val errorCode = ErrorCode.UNAUTHORIZED
         val response = ApiResponseDto.error(errorCode.message, errorCode.status, errorCode.code)
-        return ResponseEntity.ok(response)
+        return ResponseEntity.status(errorCode.status).body(response)
     }
 }
