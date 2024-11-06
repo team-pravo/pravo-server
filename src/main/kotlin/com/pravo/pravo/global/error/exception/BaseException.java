98bdb51c8eee56b0f7d9d4b8d30fe67949792721
dong2ast <kdh01132006@naver.com>
2024-11-06 23:55:04 +0900

    [CHORE] develop 브랜치와 merge

--- src/main/kotlin/com/pravo/pravo/global/error/exception/BaseException.java
+++ src/main/kotlin/com/pravo/pravo/global/error/exception/BaseException.java
@@ -12,4 +12,9 @@ public class BaseException extends RuntimeException {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
+
+    public BaseException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
