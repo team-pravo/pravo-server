81ccc64cf51c91716f8f305c6fcfc122f3ac7d51
dthcs <dthcsc@gmail.com>
2024-11-13 02:09:01 +0900

    [FEAT] apple login and add common social login

--- src/main/kotlin/com/pravo/pravo/domain/member/controller/MemberController.java
+++ src/main/kotlin/com/pravo/pravo/domain/member/controller/MemberController.java
@@ -4,11 +4,14 @@ import com.pravo.pravo.domain.member.dto.LoginRequestDTO;
 import com.pravo.pravo.domain.member.dto.LoginResponseDTO;
 import com.pravo.pravo.domain.member.service.MemberService;
 import com.pravo.pravo.global.common.ApiResponseDto;
-import com.pravo.pravo.global.oauth.kakao.dto.KakaoLoginRequestDTO;
+import com.pravo.pravo.global.oauth.apple.service.AppleService;
+import com.pravo.pravo.global.oauth.domain.SocialLoginRequestToken;
+import com.pravo.pravo.global.oauth.domain.enums.SocialProvider;
 import com.pravo.pravo.global.oauth.kakao.service.KakaoService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,10 +24,13 @@ public class MemberController {
 
     private final MemberService memberService;
     private final KakaoService kakaoService;
+    private final AppleService appleService;
 
-    public MemberController(MemberService memberService, KakaoService kakaoService) {
+    public MemberController(MemberService memberService, KakaoService kakaoService,
+        AppleService appleService) {
         this.memberService = memberService;
         this.kakaoService = kakaoService;
+        this.appleService = appleService;
     }
 
     @PostMapping("/login")
@@ -34,12 +40,15 @@ public class MemberController {
         //TODO: 삭제 필요, 테스트용 임시 로그인 API
     }
 
-    @PostMapping("/login/kakao")
-    public ApiResponseDto<LoginResponseDTO> loginKakao(
-        @RequestBody KakaoLoginRequestDTO kakaoLoginRequestDTO) {
-        LoginRequestDTO loginRequestDto = kakaoService.fetchKakaoMemberId(
-            kakaoLoginRequestDTO.getKakaoToken());
-        return ApiResponseDto.success(memberService.login(loginRequestDto));
+    @PostMapping("/login/{socialProvider}")
+    public ApiResponseDto<LoginResponseDTO> login(
+        @PathVariable SocialProvider socialProvider,
+        @RequestBody SocialLoginRequestToken socialLoginRequestToken) {
+        LoginRequestDTO loginRequestDTO = switch (socialProvider) {
+            case kakao -> kakaoService.fetchKakaoMemberId(socialLoginRequestToken.getSocialToken());
+            case apple -> appleService.fetchAppleMemberId(socialLoginRequestToken.getSocialToken());
+        };
+        return ApiResponseDto.success(memberService.login(loginRequestDTO));
     }
 
     @PostMapping("/logout")
---
+++ src/main/kotlin/com/pravo/pravo/global/oauth/domain/SocialLoginRequestToken.java
@@ -0,0 +1,14 @@
+package com.pravo.pravo.global.oauth.domain;
+
+public class SocialLoginRequestToken {
+
+    private String socialToken;
+
+    public String getSocialToken() {
+        return socialToken;
+    }
+
+    public void setSocialToken(String socialToken) {
+        this.socialToken = socialToken;
+    }
+}
