ec40fc872d97868c529458fde58954ff9a63a9b6
baebae02 <bae4614@gmail.com>
2024-11-13 17:07:53 +0900

    refactor: 204 response로 변경

--- src/main/kotlin/com/pravo/pravo/domain/promise/controller/PromiseController.kt
+++ src/main/kotlin/com/pravo/pravo/domain/promise/controller/PromiseController.kt
@@ -8,10 +8,12 @@ import com.pravo.pravo.global.auth.annotation.AuthUser
 import com.pravo.pravo.global.common.ApiResponseDto
 import com.pravo.pravo.global.jwt.AuthenticateUser
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -39,6 +41,7 @@ class PromiseController(
 
     @DeleteMapping("/{promiseId}")
     @SecurityRequirement(name = "jwt")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     override fun deletePromise(
         @PathVariable promiseId: Long,
         @AuthUser authenticatedUser: AuthenticateUser,
