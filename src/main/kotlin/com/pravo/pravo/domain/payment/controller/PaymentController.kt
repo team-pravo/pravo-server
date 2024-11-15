aa89a17d14ab1457c2dc1f2b84b9e4b546830fd9
dong2ast <kdh01132006@naver.com>
2024-11-15 23:36:58 +0900

    [REFACTOR] 파라미터 및 반환 형태 변경

--- src/main/kotlin/com/pravo/pravo/domain/payment/controller/PaymentController.kt
+++ src/main/kotlin/com/pravo/pravo/domain/payment/controller/PaymentController.kt
@@ -2,6 +2,7 @@ package com.pravo.pravo.domain.payment.controller
 
 import com.pravo.pravo.domain.payment.dto.response.RequestOrderResponseDto
 import com.pravo.pravo.domain.payment.service.PaymentFacade
+import com.pravo.pravo.global.auth.annotation.AuthUser
 import com.pravo.pravo.global.common.ApiResponseDto
 import com.pravo.pravo.global.external.toss.dto.request.ConfirmRequestDto
 import com.pravo.pravo.global.jwt.AuthenticateUser
@@ -17,14 +18,12 @@ class PaymentController(
     private val paymentFacade: PaymentFacade,
 ) : PaymentApi {
     @GetMapping
-    override fun requestOrder(authenticatedUser: AuthenticateUser): ApiResponseDto<RequestOrderResponseDto> =
-        ApiResponseDto.success(paymentFacade.requestOrder(authenticatedUser.memberId))
+    override fun requestOrder(
+        @AuthUser authenticatedUser: AuthenticateUser,
+    ): ApiResponseDto<RequestOrderResponseDto> = ApiResponseDto.success(paymentFacade.requestOrder(authenticatedUser.memberId))
 
     @PostMapping
     override fun confirmOrder(
         @RequestBody confirmRequestDto: ConfirmRequestDto,
-    ): ApiResponseDto<Unit> {
-        paymentFacade.confirmOrder(confirmRequestDto)
-        return ApiResponseDto.success()
-    }
+    ): ApiResponseDto<Unit> = ApiResponseDto.success(paymentFacade.confirmOrder(confirmRequestDto))
 }
