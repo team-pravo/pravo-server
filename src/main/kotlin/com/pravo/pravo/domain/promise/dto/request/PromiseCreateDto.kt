e3238d69d06c1f0e3ba8e2e01eeb5c4d392185e7
dong2ast <kdh01132006@naver.com>
2024-11-20 19:36:37 +0900

    [HOTFIX] 약속 영어 단어 네이밍 수정

--- src/main/kotlin/com/pravo/pravo/domain/promise/dto/request/PromiseCreateDto.kt
+++ src/main/kotlin/com/pravo/pravo/domain/promise/dto/request/PromiseCreateDto.kt
@@ -7,7 +7,7 @@ data class PromiseCreateDto(
     @Schema(description = "약속 이름", example = "약속 이름")
     val name: String,
     @Schema(description = "약속 일시", example = "2021-08-01T00:00:00")
-    val promiseDate: LocalDateTime,
+    val scheduledAt: LocalDateTime,
     @Schema(description = "약속 장소", example = "약속 장소")
     val location: String,
     @Schema(description = "예약금", example = "예약금")
