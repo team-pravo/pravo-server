76ea7a4340a05205b945b985d9190b85b61b03ac
Seo Hyeon Bae <bae4614@gmail.com>
2024-12-05 00:26:44 +0900

    hotfix: fcm config error

--- src/main/kotlin/com/pravo/pravo/global/config/FirebaseConfig.java
+++ src/main/kotlin/com/pravo/pravo/global/config/FirebaseConfig.java
@@ -6,14 +6,19 @@ import java.io.InputStream;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import com.google.firebase.FirebaseApp;
+import org.springframework.beans.factory.annotation.Value;
 import com.google.firebase.FirebaseOptions;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.config-path}")
+    private String FIREBASE_CONFIG_PATH;
+
     @PostConstruct
     public void init(){
         try{
-            InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
+            InputStream serviceAccount = new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
