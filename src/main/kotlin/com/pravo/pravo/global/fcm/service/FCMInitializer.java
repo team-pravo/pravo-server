9fc8924a9c1b55e7ccb3e907ce55bf3fc3bd36ec
Seo Hyeon Bae <bae4614@gmail.com>
2024-12-08 15:46:32 +0900

    fix: fcm initializer

--- src/main/kotlin/com/pravo/pravo/global/fcm/service/FCMInitializer.java
+++ src/main/kotlin/com/pravo/pravo/global/fcm/service/FCMInitializer.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
@@ -17,11 +18,14 @@ public class FCMInitializer {
     @PostConstruct
     public void initialize() {
         try {
-            FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(
-                    new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream()
-                ))
+            FileInputStream serviceAccount =
+                new FileInputStream("app/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
+
+            FirebaseApp.initializeApp(options);
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
