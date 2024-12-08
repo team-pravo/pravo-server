0d4b2757c24e81a491103eaa9c3b61827379f37a
Seo Hyeon Bae <bae4614@gmail.com>
2024-12-08 15:16:44 +0900

    feat: dockerfile add key

--- src/main/kotlin/com/pravo/pravo/global/config/FirebaseConfig.java
+++ src/main/kotlin/com/pravo/pravo/global/config/FirebaseConfig.java
@@ -2,7 +2,10 @@ package com.pravo.pravo.global.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import jakarta.annotation.PostConstruct;
+import java.io.File;
 import java.io.InputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import com.google.firebase.FirebaseApp;
@@ -10,10 +13,19 @@ import com.google.firebase.FirebaseOptions;
 
 @Configuration
 public class FirebaseConfig {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @PostConstruct
     public void init(){
         try{
-            InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
+            logger.info("Firebase 초기화 시작");
+            File file = new File("serviceAccountKey.json");
+            logger.info("File exists: {}", file.exists());
+            logger.info("Absolute path: {}", file.getAbsolutePath());
+
+            ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
+            logger.info("Resource exists: {}", resource.exists());
+
+            InputStream serviceAccount = resource.getInputStream();
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
